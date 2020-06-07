a3353a17f26ce244b937235a30091c052718c0c7
AlyShmahell <aly.shmahell@gmail.com>
2020-06-07 12:04:26 +0200

    fixed issue affecting mobile devices with deviceinfo

--- javacrypt.js
+++ javacrypt.js
@@ -2,7 +2,7 @@ function deviceinfo()
 {
   var parts = navigator.userAgent.split(/\s*[)(]\s*/);
   var dict = {
-    "os_name": parts[1].split(" "),
+    "os_name": parts[1].split(" ")[0].replace(/;/g, '').toLowerCase(),
     "os_version": /[0-9]+/i.exec(parts[1])[0],
     "type": /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(parts[1])? "mobile" : "desktop",
     "app_version": /[0-9]+/i.exec(parts[0])[0]
